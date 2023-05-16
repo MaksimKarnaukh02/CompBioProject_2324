2849487dfb1d8b7672b682d500db728a878f4a30
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2023-05-16 05:12:49 +0200

    python tree traversing example code works (was imported in main to test)

--- python_tree_traversing.py
+++ python_tree_traversing.py
@@ -1,23 +1,26 @@
 from Bio import Phylo
-tree=Phylo.read(r"inversion_samples_noninverted_pwd_nj_tree_ancestral_rooted.newick.nwk")
-# now tree is a root node of the tree
-# let's get all internal nodes:
-nodes=tree.get_nonterminals()
 
-# we can take random node
-node=nodes[42]
 
-# and then we can get it's OTUs (leaves, terminals, tips here are synonyms)
-# as nodes
-OTUS=node.get_terminals()
-# or just labels/names of this terminals
-print([n.name for n in OTUS])
+def tt():
+    tree = Phylo.read(r"inversion_samples_noninverted_pwd_nj_tree_ancestral_rooted.newick.nwk", "newick")
+    # now tree is a root node of the tree
+    # let's get all internal nodes:
+    nodes = tree.get_nonterminals()
 
-# you can also get children of a node
-c1,c2=node.clades
-print([n.name for n in c1.get_terminals()])
-print([n.name for n in c2.get_terminals()])
+    # we can take random node
+    node = nodes[42]
 
-# you can compare nodes to find if they're the same node
-print([n for n in nodes if n==c1])
-print([n for n in nodes if n==c2])
+    # and then we can get it's OTUs (leaves, terminals, tips here are synonyms)
+    # as nodes
+    OTUS = node.get_terminals()
+    # or just labels/names of this terminals
+    print([n.name for n in OTUS])
+
+    # you can also get children of a node
+    c1, c2 = node.clades
+    print([n.name for n in c1.get_terminals()])
+    print([n.name for n in c2.get_terminals()])
+
+    # you can compare nodes to find if they're the same node
+    print([n for n in nodes if n == c1])
+    print([n for n in nodes if n == c2])
