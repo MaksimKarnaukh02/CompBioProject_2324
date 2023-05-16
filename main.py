63a3b529bd33d14bcc6aa4afe5e2e0595c476de2
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2023-05-16 05:04:09 +0200

    get chr length function

--- main.py
+++ main.py
@@ -59,22 +59,39 @@ def get_accessible_size(accessible_fn, chrom, start=None, end=None):
         access = 0
     return access
 
-def get_good_windows(accessible_fn, chrom, window_size=100000):
+
+def get_chr_length(reference_genome, chrom):
+    """
+    Returns the length of the given chromosome using a reference genome.
+    """
+    with open(reference_genome, 'r') as file:
+        for line in file:
+            if line.startswith(f"@SQ\tSN:{chrom}"):
+                length_field = line.split('\t')[1]
+                chrom_len = int(length_field.split(':')[1])
+                return chrom_len
+    raise ValueError(f"Chromosome '{chrom}' not found in the reference genome file.")
+
+
+def get_good_windows(accessible_fn, chrom, window_size=100000, reference_genome=None):
     """
     Returns a list of (start, end) tuples of windows of size window_size
     that have at least 90% of sites accessible in the given chromosome.
     """
-    chrom_len = get_accessible_size(accessible_fn, chrom)
+    chrom_len: int = get_chr_length(reference_genome, chrom) # 41162407 in file 'GCA_900246225.3_fAstCal1.2_genomic_chromnames_mt.dict' for chr1
     good_windows = []
     for start in range(0, chrom_len, window_size):
         end = min(start + window_size, chrom_len)
-        if get_accessible_size(accessible_fn, chrom, start, end) / (end - start) > 0.9:
+        if get_accessible_size(accessible_fn, chrom, start, end) / (end - start) > 0.9: # # of accessible sites / window size
             good_windows.append((start, end))
     return good_windows
 
+
 def main():
-    pass
+
     # get_good_windows(".bed.gz", "chr1")
+
+    return 0
 
 
 if __name__ == "__main__":
