97c3ca3b1cd6a40e2c13e84252a28d3a58fa592c
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2023-05-19 01:22:19 +0200

    update

--- src/VCFFilter.sh
+++ src/VCFFilter.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-selected_individuals="selected_individuals.txt"
+selected_individuals="individuals.tsv"
+selected_individuals_txt="individuals.txt"
 
-vcf_file="malawi_cichlids_v3_phase.biallelic_snps.pass.ancestral_as_sample.benthic.chr1.vcf.gz"
-bed_file="goodwindows.bed"
-output_file="filtered.vcf"
+vcf_file="/scratch/antwerpen/208/vsc20811/2024-05_compbio_project/malawi_cichlids_v3_phase.biallelic_snps.pass.ancestral_as_sample.benthic.chr1.vcf.gz"
+bed_file="chr1_windows.bed"
+output_file="${VSC_DATA}/filtered.vcf.gz"
+finaloutput_file="${VSC_DATA}/finaloutput.vcf.gz"
 
-# Filter VCF using BED file
-bedtools intersect -a "$vcf_file" -b "$bed_file" -header > "$output_file"
+echo "filter VCF file using BED file"
+# Filter VCF using BED file and redirect output to gzip-compressed VCF file
+bedtools intersect -a "$vcf_file" -b "$bed_file" -header | gzip -c > "$output_file"
+echo "VCF file filtered using BED file and saved as compressed VCF"
 
+echo "convert TSV file with selected individuals to TXT format"
+awk -F'\t' 'NR>1 {print $2; print $3}' "$selected_individuals" > "$selected_individuals_txt"
+echo "TSV file with selected individuals converted to TXT format"
+
+echo "filter VCF file further based on selected individuals"
 # Filter VCF further based on selected individuals
-bcftools view -S "$selected_individuals" "$output_file" -o finaloutput.vcf
+bcftools view -S "$selected_individuals_txt" "$output_file" | gzip "$finaloutput_file"
+echo "VCF file filtered further based on selected individuals"
+echo "Final output VCF file compressed using gzip"
