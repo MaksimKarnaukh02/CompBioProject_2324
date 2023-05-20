d5bb4266822e5ee5f35cbd0ff299cb7b7a4d0007
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2023-05-21 00:00:32 +0200

    update

---
+++ scripts/PullFromCluster.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# This script will push the files to the cluster
+rsync -av -r -P --exclude 'scripts' --exclude 'win_venv' --exclude '.idea' --exclude '.git' vsc20886@login.hpc.uantwerpen.be:/scratch/antwerpen/208/vsc20886/CompBioProject_2324/* ./
