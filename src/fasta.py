b4c0eb4b54c4c1c0c72783af5273cdc613bc5561
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2023-05-21 23:13:09 +0200

    update

--- src/fasta.py
+++ src/fasta.py
@@ -1,8 +1,10 @@
 import subprocess
 import os
+
+
 # from pyfaidx import FastaVariant
 
-def clip_reference_genome(ref_genome_file: str, vcf_file:str, output_file:str):
+def clip_reference_genome(ref_genome_file: str, vcf_file: str, output_file: str):
     """
     Clip the reference genome to the same extent as the VCF file.
 
@@ -26,6 +28,7 @@ def clip_reference_genome(ref_genome_file: str, vcf_file:str, output_file:str):
     #         print(record, file=out)
     # out.close()
 
+
 def split_fasta_file(fasta_file, window_size, output_directory):
     """
     Split a large FASTA file into smaller windows of specified size.
@@ -35,7 +38,7 @@ def split_fasta_file(fasta_file, window_size, output_directory):
         window_size (int): Size of the window in base pairs.
         output_directory (str): Path to the output directory to store the windowed FASTA files (Output).
     """
-
+    print(f"Splitting FASTA file into windows with windows size {window_size}...")
     # Split the large FASTA file into smaller windows
     with open(fasta_file, 'r') as input_file:
         sequence = ''
@@ -49,7 +52,7 @@ def split_fasta_file(fasta_file, window_size, output_directory):
                 sequence += line.strip()
         if sequence:
             write_fasta_window(sequence, window_size, output_directory)
-
+    print("\tDone.")
 
 def write_header(header, output_directory):
     """
@@ -81,19 +84,25 @@ def write_fasta_window(sequence, window_size, output_directory):
             output_file.write(window)
 
 
+VSC_DATA = '/data/antwerpen/208/vsc20886'
+VSC_SCRATCH = '/scratch/antwerpen/208/vsc20886'
+
 if __name__ == '__main__':
     # Define the input and output file paths
     # Reference genome FASTA file (Input)
-    ref_genome_file = "/scratch/antwerpen/208/vsc20811/2024-05_compbio_project/genome/GCA_900246225.3_fAstCal1.2_genomic_chromnames_mt.fa"
-    vcf_file = os.path.expandvars('${VSC_DATA}/filtered.vcf.gz')   # VCF file (Input)
-    output_file = os.path.expandvars('${VSC_DATA}/clipped_reference.fa')  # Output file for the clipped reference genome (Output)
+    ref_genome_file = f"/scratch/antwerpen/208/vsc20811/2024-05_compbio_project/genome/GCA_900246225.3_fAstCal1.2_genomic_chromnames_mt.fa"
+    vcf_file = f'{VSC_DATA}/filtered.vcf.gz'  # VCF file (Input)
+    output_file = f'{VSC_DATA}/clipped_reference.fa'  # Output file for the clipped reference genome (Output)
 
     # Clip the reference genome
     clip_reference_genome(ref_genome_file, vcf_file, output_file)
 
     # Define the input and output file paths
     window_size = 100000  # Size of the window in base pairs
-    output_directory = os.path.expandvars('${VSC_DATA}/windowed_fastas')  # Output directory to store the windowed FASTA files (Output)
+    output_directory = f'{VSC_DATA}/windowed_fastas'  # Output directory to store the windowed FASTA files (Output)
+    # Create the output directory if it does not exist
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
     # Split the FASTA file into smaller windows
     split_fasta_file(output_file, window_size, output_directory)
