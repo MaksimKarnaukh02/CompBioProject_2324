b81b79482c8448f0eabd0bc67e33543a903bac20
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2023-05-22 19:00:57 +0200

    update

--- src/vcf2fasta.sh
+++
@@ -1,6 +0,0 @@
-vcf_file=".vcf.gz"
-fasta_file="/scratch/antwerpen/208/vsc20811/2024-05_compbio_project/genome/GCA_900246225.3_fAstCal1.2_genomic_chromnames_mt.fa"
-
-#cat "$fasta_file" | vcf-consensus "$vcf_file" > output.fa
-
-bcftools consensus -f "$fasta_file" "$vcf_file" > out.fa
