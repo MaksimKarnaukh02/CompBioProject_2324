3f1d55b7fb60d26de67b9723edba09e148b7fa26
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2023-05-23 17:28:14 +0200

    update

--- src/vcf_to_fasta.py
+++ src/vcf_to_fasta.py
@@ -2,23 +2,34 @@ import vcf
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from pprint import pprint
+import inspect
 
 def vcf_to_fasta(vcf_file, ref_fasta, output_fasta): #TODO test if actually works
     # Read the reference genome FASTA file
+    print('Reading reference genome FASTA file')
     ref_records = SeqIO.to_dict(SeqIO.parse(ref_fasta, 'fasta'))
+    print('Finished reading reference genome FASTA file')
+    vcf_reader = vcf.Reader(open(vcf_file, 'rb')) # Open the VCF file
 
-    vcf_reader = vcf.Reader(open(vcf_file, 'r')) # Open the VCF file
     seq_records = [] # Create a list to store the resulting SeqRecord objects
 
+    print('Processing VCF file')
     for record in vcf_reader: # Process each record in the VCF file
+        # print(f"record: {record}")
+
         chrom = record.CHROM
+        # print(f"chrom: {chrom}")
         pos = record.POS
+        # print(f"pos: {pos}")
+
         ref_allele = record.REF
-
+        # print(f"ref_allele: {ref_allele}")
         ref_sequence = str(ref_records[chrom].seq) # Get the reference sequence for the current chromosome
-
+        # print(f"ref_sequence: {ref_sequence}")
         seq = Seq(ref_sequence)
         seq = seq.tomutable()
+
         seq[pos - 1] = ref_allele # set to reference allele by default
 
         for sample in record.samples:
@@ -39,12 +50,13 @@ def vcf_to_fasta(vcf_file, ref_fasta, output_fasta): #TODO test if actually work
                 seq_records.append(seq_record)
                 break # break because we only look at the first number, so before the | or /
 
+    print('Finished processing VCF file')
     SeqIO.write(seq_records, output_fasta, 'fasta') # Save the SeqRecord objects to a FASTA file
-
+    print('Finished writing FASTA file')
 if __name__ == '__main__':
+    vcf_file = "/data/antwerpen/208/vsc20886/finaloutput.vcf.gz"
+    ref_fasta = "/scratch/antwerpen/208/vsc20811/2024-05_compbio_project/genome/GCA_900246225.3_fAstCal1.2_genomic_chromnames_mt.fa"
 
-    vcf_file = 'input.vcf'
-    ref_fasta = 'reference.fasta'
-    output_fasta = 'output.fasta'
+    output_fasta = '/data/antwerpen/208/vsc20886/output.fasta'
 
     vcf_to_fasta(vcf_file, ref_fasta, output_fasta)
