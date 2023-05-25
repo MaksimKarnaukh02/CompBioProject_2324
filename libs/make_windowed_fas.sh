6c91de301ff179d66c8d089a383907b0b118775f
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2023-05-25 20:43:26 +0200

    Changes by Maksim Karnaukh

--- libs/make_windowed_fas.sh
+++ libs/make_windowed_fas.sh
@@ -17,23 +17,26 @@ fi
 #  exit 1;
 #fi
 
-
 # !!! doesn't affect rehydration part (TODO: fix)
 window_size=100000
+total_windows=$(((41162407+ window_size / 2)/window_size)) # chrom len = 41 162 407
+#  window_n=${1}
 
-window_n=${1}
-window_start=$((window_size*window_n))
-window_end=$((window_start + window_size))
-window_start_1_based=$((window_start + 1))
+for ((window_n=0; window_n<total_windows; window_n++)); do
 
-window_VCF="${temp_dir}/window.chr1.${window_start}.vcf.gz"
-vcf2phylip_prefix="${temp_dir}/window.chr1.${window_start}.aln"
+  window_start=$((window_size*window_n))
+  window_end=$((window_start + window_size))
+  window_start_1_based=$((window_start + 1))
 
-gatk SelectVariants -R ${ref} -V ${vcf} -L chr1:${window_start_1_based}-${window_end} -O ${window_VCF}
-python3 ../scripts/vcf2phylip.py -i ${window_VCF} --output-prefix ${vcf2phylip_prefix} -f -w
-python3 ../scripts/rehydrate_vcf_to_fas.py ${vcf2phylip_prefix}.min4.fasta ${vcf2phylip_prefix}.min4.used_sites.tsv ${cut_ref} ${window_start}
+  window_VCF="${temp_dir}/window.chr1.${window_start}.vcf.gz"
+  vcf2phylip_prefix="${temp_dir}/window.chr1.${window_start}.aln"
 
-cp "${vcf2phylip_prefix}.min4.rehydrated.fasta" .
+  gatk SelectVariants -verbosity:ERROR -R ${ref} -V ${vcf} -L chr1:${window_start_1_based}-${window_end} -O ${window_VCF}
+  python3 libs/vcf2phylip.py -i ${window_VCF} --output-prefix ${vcf2phylip_prefix} -f -w
+  python3 libs/rehydrate_vcf_to_fas.py ${vcf2phylip_prefix}.min4.fasta ${vcf2phylip_prefix}.min4.used_sites.tsv ${cut_ref} ${window_start}
+
+  cp "${vcf2phylip_prefix}.min4.rehydrated.fasta" .
+done
 
 echo "About to rm ${temp_dir}"
 #rm -rf "${temp_dir}"
